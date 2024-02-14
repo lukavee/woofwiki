b86ccd8108b81ffc032f7debc55838db5750b9b4
Luka Veljkovic <lukaposlovni23@gmail.com>
2024-02-14 22:13:46 +0100

    More styles added

--- src/Breed.jsx
+++ src/Breed.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Breed = ({breedInfo}) => {
   return (
     <div className='dog-info-container'>
-      <h2>{breedInfo.name}</h2>
-      <p>{breedInfo.description}</p>
+      <h2>Breed name: {breedInfo.name}</h2>
+      <p>Characteristics: <span>{breedInfo.description}</span></p>
       <img src={breedInfo.image} alt={breedInfo.name} />
     </div>
   )
